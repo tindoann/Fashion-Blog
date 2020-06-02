a731168c8df5d316b79eee0f7ee873e57fe6018f
tindoann <tindoann@gamil.com>
2020-06-02 17:38:27 -0400

    site is render but not sidebar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,38 +1,44 @@
-import React, {useState} from 'react'; 
-import {Navlink} from 'react-router-dom'; 
-import './style.css'; 
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import './style.css';
 
 const Navbar = (props) => {
 
-  const [search, setSearch] = useState(false); 
+    const [search, setSearch] = useState(false);
 
-  const submitSearch = (e) => {
-    e.preventDefault(); 
-    alert('Searched')
-  }
 
-  const openSearch = () => {
-    setSearch(true); 
-  }
+    const submitSearch = (e) => {
+        e.preventDefault();
+        alert('Searhed');
 
-  const searchClass = search ? 'searchInput active' : 'searchInput'; 
+    }
 
-  return (
-    <div className='navbar'>
-      <ul className='navbarMenu'>
-        <li><Navlink to='/'>Home</Navlink></li>
-        <li><Navlink to='/about-us'>About Us</Navlink></li>
-        <li><Navlink to='/post'></Navlink>Posts</li>
-        <li><Navlink to='/contact'></Navlink>Contact Us</li>
-      </ul>
-      <div className='search'>
-        <form onsubmit={submitSearch}>
-          <input type='text' className={searchClass} placeholder='Search' />
-          <img onClick={openSearch} className="searchIcon" src={require('../../assets/icons/search.png')} alt="Search" />
-        </form>
-      </div>
+
+    const openSearch = () => {
+        setSearch(true);
+    }
+
+
+    const searchClass = search ? 'searchInput active' : 'searchInput';
+
+  return(
+    <div className="navbar">
+        <ul className="navbarMenu">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/about-us">About Us</NavLink></li>
+            <li><NavLink to="/post">Posts</NavLink></li>
+            <li><NavLink to="/contact-us">Contact Us</NavLink></li>
+        </ul>
+        <div className="search">
+            <form onSubmit={submitSearch}>
+                <input type="text" className={searchClass} placeholder="Search" />
+                {/* <img onClick={openSearch} className="searchIcon" src={require('../../assets/icons/search.png')} alt="Search" /> */}
+            </form>
+            
+        </div>
     </div>
-  )
-}
+   )
+
+ }
 
 export default Navbar
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
 import Card from '../UI/Card';
-
 import blogPost from '../../data/blog.json';
 import { NavLink } from 'react-router-dom';
 
 const Sidebar = (props) => {
 
     const [posts, setPosts] = useState([]);
-    
     
     useEffect(() => {
         const posts = blogPost.data;
@@ -17,53 +15,55 @@ const Sidebar = (props) => {
 
 
 
-  return(
-      <div className="sidebarContainer" {...props}>
-            <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }}>
-                <div className="cardHeader">
-                    <span>About Us</span>
-                </div>
-                <div className="profileImageContainer">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRGAb43NxZswWRLw9RoxC8YwykXlLy_efq2r674Xj0ffRR3D-0P" alt="" />
-                </div>
-                <div className="cardBody">
-                    <p className="personalBio">My name is Tin:</p>
-                </div>
-            </Card>
-
-            <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }}>
-                <div className="cardHeader">
-                    <span>Social Network</span>
-                </div>
-            </Card>
-
-            <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }}>
-                <div className="cardHeader">
-                    <span>Recent Posts</span>
-                </div>
-
-                <div className="recentPosts">
-
-                    {
-                        posts.map(post => {
-                            return (
-                                <NavLink key={post.id} to={`/post/${post.slug}`}>
-                                    <div className="recentPost">
-                                        <h3>{post.blogTitle}</h3>
-                                        <span>{post.postedOn}</span>
-                                    </div>
-                                </NavLink>
-                                
-                            );
-                        })
-                    }
-                </div>
-
-            </Card>
-      </div>
-    
-    )
-
- }
-
-export default Sidebar
+    return(
+        <div className="sidebarContainer" style={{
+            width: props.width
+        }}>
+              <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }}>
+                  <div className="cardHeader">
+                      <span>About Us</span>
+                  </div>
+                  <div className="profileImageContainer">
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRGAb43NxZswWRLw9RoxC8YwykXlLy_efq2r674Xj0ffRR3D-0P" alt="" />
+                  </div>
+                  <div className="cardBody">
+                      <p className="personalBio">My name is Tin:</p>
+                  </div>
+              </Card>
+  
+              <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }}>
+                  <div className="cardHeader">
+                      <span>Social Network</span>
+                  </div>
+              </Card>
+  
+              <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }}>
+                  <div className="cardHeader">
+                      <span>Recent Posts</span>
+                  </div>
+  
+                  <div className="recentPosts">
+  
+                      {
+                          posts.map(post => {
+                              return (
+                                  <NavLink key={post.id} to={`/post/${post.slug}`}>
+                                      <div className="recentPost">
+                                          <h3>{post.blogTitle}</h3>
+                                          <span>{post.postedOn}</span>
+                                      </div>
+                                  </NavLink>
+                                  
+                              );
+                          })
+                      }
+                  </div>
+  
+              </Card>
+        </div>
+      
+     )
+  
+   }
+  
+  export default Sidebar
